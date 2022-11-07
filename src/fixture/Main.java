2d26be32af455f4f362e5c57ec98e4f567b58288
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2022-11-07 15:52:17 -0300

    Fixture
    Parte 1

---
+++ src/fixture/Main.java
@@ -0,0 +1,88 @@
+package fixture;
+
+import java.util.LinkedList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		LinkedList<Pais> grupo_a = new LinkedList <Pais>();
+		
+		grupo_a.add(new Pais ("Qatar",0,true,"A"));
+		grupo_a.add(new Pais ("Ecuador",0,true,"A"));
+		grupo_a.add(new Pais ("Senegal",0,true,"A"));
+		grupo_a.add(new Pais ("Paises_bajos",0,true,"A"));
+		
+		
+		LinkedList<Pais> grupo_b = new LinkedList <Pais>();
+		
+		grupo_a.add(new Pais ("Qatar",0,true,"B"));
+		grupo_a.add(new Pais ("Ecuador",0,true,"B"));
+		grupo_a.add(new Pais ("Senegal",0,true,"B"));
+		grupo_a.add(new Pais ("Paises_bajos",0,true,"B"));
+		
+		
+		
+		
+		LinkedList<Pais> grupo_c = new LinkedList <Pais>();
+		
+		grupo_a.add(new Pais ("Qatar",0,true,"C"));
+		grupo_a.add(new Pais ("Ecuador",0,true,"C"));
+		grupo_a.add(new Pais ("Senegal",0,true,"C"));
+		grupo_a.add(new Pais ("Paises_bajos",0,true,"C"));
+		
+		
+		
+		
+		LinkedList<Pais> grupo_d = new LinkedList <Pais>();
+		
+		grupo_a.add(new Pais ("Qatar",0,true,"D"));
+		grupo_a.add(new Pais ("Ecuador",0,true,"D"));
+		grupo_a.add(new Pais ("Senegal",0,true,"D"));
+		grupo_a.add(new Pais ("Paises_bajos",0,true,"D"));
+		
+		
+		
+		LinkedList<Pais> grupo_e = new LinkedList <Pais>();
+		
+		grupo_a.add(new Pais ("Qatar",0,true,"E"));
+		grupo_a.add(new Pais ("Ecuador",0,true,"E"));
+		grupo_a.add(new Pais ("Senegal",0,true,"E"));
+		grupo_a.add(new Pais ("Paises_bajos",0,true,"E"));
+		
+		
+		
+		LinkedList<Pais> grupo_f = new LinkedList <Pais>();
+		
+		grupo_a.add(new Pais ("Qatar",0,true,"F"));
+		grupo_a.add(new Pais ("Ecuador",0,true,"F"));
+		grupo_a.add(new Pais ("Senegal",0,true,"F"));
+		grupo_a.add(new Pais ("Paises_bajos",0,true,"F"));
+		
+		
+		
+		LinkedList<Pais> grupo_g = new LinkedList <Pais>();
+		
+		grupo_a.add(new Pais ("Qatar",0,true,"G"));
+		grupo_a.add(new Pais ("Ecuador",0,true,"G"));
+		grupo_a.add(new Pais ("Senegal",0,true,"G"));
+		grupo_a.add(new Pais ("Paises_bajos",0,true,"G"));
+		
+		
+		
+		LinkedList<Pais> grupo_h = new LinkedList <Pais>();
+		
+		grupo_a.add(new Pais ("Qatar",0,true,"G"));
+		grupo_a.add(new Pais ("Ecuador",0,true,"G"));
+		grupo_a.add(new Pais ("Senegal",0,true,"G"));
+		grupo_a.add(new Pais ("Paises_bajos",0,true,"G"));
+		
+		
+		
+		
+		
+
+	}
+
+}
